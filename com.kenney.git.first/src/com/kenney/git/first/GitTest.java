f985150c776e751928b23871dee5b1a211f9f5ac
Kenney Ley <Kenney.Ley32@myhunter.cuny.edu>
2021-08-30 15:27:45 -0400

    First adjustment to commit

---
+++ com.kenney.git.first/src/com/kenney/git/first/GitTest.java
@@ -0,0 +1,12 @@
+package com.kenney.git.first;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Learning git right now");
+		System.out.println("to save practices");
+		System.out.println("on github");
+	}
+
+}
